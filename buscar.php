69ec6389034a08c33873e7cd94776ab1a93a6742
moavalos <mora_m_avalos@hotmail.com>
2024-05-19 13:45:27 -0300

    preparo database y demas

--- buscar.php
+++ buscar.php
@@ -1,11 +1,40 @@
 <?php
+include_once("Database.php");
 $config = parse_ini_file("config.ini");
 
 $servername = $config["servername"];
 $usuario = $config["usuario"];
-$contraseña = $config[""];
+$contrasena = $config[""];
 $database = $config["database"];
 
-$database = new Database($servername, $usuario, $contraseña, $database);
-$pokemon = $database->query("SELECT * FROM pokemon");
+$database = new Database($servername, $usuario, $contrasena, $database);
+//$pokemon = $database->query("SELECT * FROM pokemon");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $dataPokemon = $_POST["buscar"];
+
+    $stmt = $database->prepare("SELECT * FROM pokemon WHERE nombre=? OR tipo_id=? OR tipo2_id=?");
+    $stmt->bind_param("sss", $dataPokemon, $dataPokemon, $dataPokemon);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    $stmt->close();
+
+    if ($resultado->num_rows > 0) {
+        $row = $resultado->fetch_assoc();
+        header("Location: detalle.php?id=" . $row["id"]);
+        exit();
+    } else {
+        echo "No se encontró ningún Pokémon con el nombre o número proporcionado.<br>";
+
+        $result = $database->query("SELECT * FROM pokemon");
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo 'Pokemon n°' . $row["id"] . ", nombre :" . $row["nombre"] . ", tipo:" . $row["tipo_id"] . ".<br>";
+            }
+        } else {
+            echo "No hay ningún Pokémon en la base de datos.";
+        }
+    }
+}
+
 ?>
--- helper/Database.php
+++ helper/Database.php
@@ -32,38 +32,3 @@ class Database
     }
 
 }
-
-/*$conexion = mysqli_connect($servername, $usuario, $contraseña, $database);
-
-if ($conexion->connect_error) {
-    die("Error en la conexión: " . $conexion->connect_error);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $dataPokemon = $_POST["buscar"];
-
-    $stmt = $conexion->prepare("SELECT * FROM pokemon WHERE nombre=? OR tipo_id = ? OR tipo2_id = ?");
-    $stmt->bind_param("sss", $dataPokemon, $dataPokemon, $dataPokemon);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
-    $stmt->close();
-
-    if ($resultado->num_rows > 0) {
-        $row = $resultado->fetch_assoc();
-        header("Location: detalle.php?id=" . $row["id"]);
-        exit();
-    } else {
-        echo "No se encontró ningún Pokémon con el nombre o número proporcionado.<br>";
-
-        $result = mysqli_query($conexion, "SELECT * FROM pokemon");
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo 'Pokemon n°' . $row["id"] . ", nombre :" . $row["nombre"] . ", tipo:" . $row["tipo_id"] . ".<br>";
-            }
-        } else {
-            echo "No hay ningún Pokémon en la base de datos.";
-        }
-    }
-}
-
-$conexion->close();*/
